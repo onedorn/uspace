e23dbfc1cec0838511b06353f9b0d5e5dc7ad104
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2024-04-18 23:24:06 +0300

    [USpace] Basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import React, { JSX } from 'react';
 import './App.css';
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ROUTES } from './constants/routes-constants';
+
+import HomePage from './pages/HomePage';
+import NotFoundPage from './pages/NotFoundPage';
+
 function App(): JSX.Element {
-  return <div className="App">USpace</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path={ROUTES.HOMEPAGE_ROUTE} element={<HomePage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
