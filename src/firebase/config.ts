6bbfe36e697c8b3829910c5bfa04fb617f9175c2
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2024-05-07 17:04:55 +0300

    [USpace] Github actions workflow

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -5,16 +5,15 @@ import { Firestore, getFirestore } from 'firebase/firestore';
 import { FirebaseStorage, getStorage } from 'firebase/storage';
 import { getMessaging, Messaging } from 'firebase/messaging';
 import { getRemoteConfig, RemoteConfig } from 'firebase/remote-config';
-import env from 'react-dotenv';
 
 export const app: FirebaseApp = initializeApp({
-  apiKey: env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 });
 
 export const analytics: Analytics = getAnalytics(app);
