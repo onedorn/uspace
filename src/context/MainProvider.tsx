18c6dc0a4cb853556ccb77330090abbcc842ceeb
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2024-11-10 19:01:15 +0200

    [USpace] Firebase context structure

---
+++ src/context/MainProvider.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { FirebaseProvider } from './FirebaseContext';
+import { FirestoreProvider } from './FirestoreContext';
+import { StorageProvider } from './StorageContext';
+import { FunctionsProvider } from './FunctionsContext';
+import { AuthProvider } from './AuthContenxt';
+
+const MainProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => (
+  <FirebaseProvider>
+    <FirestoreProvider>
+      <StorageProvider>
+        <FunctionsProvider>
+          <AuthProvider>
+            {children}
+          </AuthProvider>
+        </FunctionsProvider>
+      </StorageProvider>
+    </FirestoreProvider>
+  </FirebaseProvider>
+);
+
+export default MainProvider;
--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
-import { FirebaseProvider } from './context/FirebaseContext';
-import { FirestoreProvider } from './context/FirestoreContext';
-import { StorageProvider } from './context/StorageContext';
+import MainProvider from './context/MainProvider';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <FirebaseProvider>
-      <FirestoreProvider>
-        <StorageProvider>
-          <App></App>
-        </StorageProvider>
-      </FirestoreProvider>
-    </FirebaseProvider>
+    <MainProvider>
+      <App />
+    </MainProvider>
   </React.StrictMode>
 );
